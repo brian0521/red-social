a5369beddc4a90f99a039c9441a05505d7cc4f07
brian0521 <brian05_21@outlook.com>
2018-09-27 17:56:58 -0500

    login

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,7 +6,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Dashboard</div>
+                <div class="card-header">Bienvenido</div>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -14,7 +14,7 @@
                             {{ session('status') }}
                         </div>
                     @endif
-                    You are logged in!
+                    Bienvenido!
                 </div>
             </div>
         </div>
