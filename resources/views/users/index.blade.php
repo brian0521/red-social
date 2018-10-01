2b199140f7682be7bc260c07cf3697255dfde100
juanfer3 <jfvilladiego2@misena.edu.co>
2018-10-01 17:46:48 -0500

    List Users

---
+++ resources/views/users/index.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <table class="table">
+  <thead>
+    <tr>
+      <th scope="col">First</th>
+      <th scope="col">Last</th>
+      <th scope="col">Handle</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr<
+  </tbody>
+</table>
+
+
+@endsection
