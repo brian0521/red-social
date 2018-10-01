547b3288400dca5101bb348521bb12f5c4d8af18
brian0521 <brian05_21@outlook.com>
2018-10-01 17:32:44 -0500

    login

--- routes/web.php
+++ routes/web.php
@@ -10,11 +10,17 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::resource('user', 'UserController');
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
 
 
