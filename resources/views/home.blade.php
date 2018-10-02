fb8db6058892d6248d00faa4c85ede1e3b63ad25
brian0521 <brian05_21@outlook.com>
2018-10-02 09:08:48 -0500

    login view

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,12 +1,11 @@
 @extends('layouts.app')
 
 @section('content')
-
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Bienvenido</div>
+                <div class="card-header">Dashboard</div>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -14,7 +13,8 @@
                             {{ session('status') }}
                         </div>
                     @endif
-                    Bienvenido! hola
+
+                    You are logged in!
                 </div>
             </div>
         </div>
